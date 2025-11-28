936a3dbe44370adc5719b5c3a311ec1e61a0c3af
Logan Williams <logan@fireflysoftware.dev>
2025-11-28 06:43:28 -0700

    feat: dedicated HomeHandler for storefront

---
+++ internal/handler/storefront/home.go
@@ -0,0 +1,31 @@
+package storefront
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// HomeHandler handles the homepage
+type HomeHandler struct {
+	templates *template.Template
+}
+
+// NewHomeHandler creates a new home handler
+func NewHomeHandler(templates *template.Template) *HomeHandler {
+	return &HomeHandler{
+		templates: templates,
+	}
+}
+
+// ServeHTTP handles GET /
+func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement homepage with featured products, hero section
+	data := map[string]interface{}{
+		"Year": 2024,
+	}
+
+	if err := h.templates.ExecuteTemplate(w, "layout", data); err != nil {
+		http.Error(w, "Failed to render template", http.StatusInternalServerError)
+		return
+	}
+}
